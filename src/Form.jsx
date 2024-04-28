921f7952f5b4d045d9c4288e7d2fc239e0ae2fc0
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-04-28 14:42:34 +0330

    states of form get

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,25 +1,61 @@
 import styles from "./Form.module.css";
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
-  const clickHandler = () => {};
+  ///////////////////////////////////////////تعریف states////////////////////////////
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-  console.log(styles);
-
+  //////////////////////////////////////////////استفاده از setState//////////////////
+  const clickHandler = () => {
+    console.log({ name, lastname, email, phone });
+  };
+  const nameHandler = (event) => {
+    setName(event.target.value);
+  };
+  const lastnameHandler = (event) => {
+    setLastname(event.target.value);
+  };
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+  const phoneHandler = (event) => {
+    setPhone(event.target.value);
+  };
+  //////////////////////////////////////////////////////////////////////////////////////
   return (
     <div className={styles.container}>
       <div className={styles.parent}>
         <div className={styles.child}>
-          <input className={styles.element} type="text" placeholder="name" />
           <input
             className={styles.element}
+            onChange={nameHandler}
             type="text"
-            placeholder="lastname"
+            placeholder="name"
+          />
+          <input
+            className={styles.element}
+            onChange={lastnameHandler}
+            type="text"
+            placeholder="last name"
           />
         </div>
         <div className={styles.child}>
-          <input className={styles.element} type="email" placeholder="email" />
-          <input className={styles.element} type="text" placeholder="phone" />
+          <input
+            className={styles.element}
+            onChange={emailHandler}
+            type="email"
+            placeholder="email"
+          />
+          <input
+            className={styles.element}
+            onChange={phoneHandler}
+            type="text"
+            placeholder="phone"
+          />
         </div>
         <button className={styles.button} onClick={clickHandler}>
           add contact
